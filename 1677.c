b0f4a40b34f37953fe304e93ccc6742757cb08ed
Han2 <randirao4245@gmail.com>
2024-08-04 23:41:39 +0900

    1677

---
+++ 1677.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for(int i=0; i<m; i++){
+        if(i==0 || i==m-1){
+            for(int j=0; j<n; j++){
+            if(j==0 || j==n-1) printf("+");
+            else printf("-");
+            }
+            printf("\n\n");
+        }
+        else{
+            for(int j=0; j<n; j++){
+                if(j==0 || j==n-1) printf("|");
+                else printf(" ");
+            }
+            printf("\n\n");
+        }
+    }
+    return 0;
+}
