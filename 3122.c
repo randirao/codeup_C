598c57d1d25cfade6b8510d005aff9ab7f339f6f
Han2 <randirao4245@gmail.com>
2024-08-06 23:43:38 +0900

    3122

--- 3122.c
+++ 3122.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main() {
     int n;
     scanf("%d", &n);
-    for(int i = 1; i <= 2*n;i++){
-        for(int j = 1; j<= 2*n; j++){
-            if(i+j == n+1) printf(" ");
-            else if(i+j == n+ 2*i ) printf(" ");
-            else if(i+j == n+ 2*j ) printf(" ");
-            else if(i+j == 3*n+1 ) printf(" ");
-            else printf("*");
-        }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) printf(" ");
+        for (int j = 0; j < 2 * i + 1; j++) printf("*");
+        printf("\n");
+    }
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j < n - i - 1; j++) printf(" ");
+        for (int j = 0; j < 2 * i + 1; j++) printf("*");
         printf("\n");
     }
     return 0;
