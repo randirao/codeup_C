9fe06b7408fe0645bb442c225946d642e8add087
Han2 <randirao4245@gmail.com>
2024-08-06 23:41:06 +0900

    1425

---
+++ 1425.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n, c, arr[100];
+    scanf("%d %d", &n, &c); //9 6
+    for(int i=1; i<=n; i++) scanf("%d", &arr[i]);
+    
+    for(int i=0;i<n-1;i++) {
+    int min=i;
+    for(int j=i+1;j<n;j++) {
+        if(arr[min] > arr[j]) min = j;
+    }
+    int tmp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = tmp;
+}
+    for(int i=1; i<=n; i++){
+        printf("%d ", arr[i]);
+        if(i%c == 0) printf("\n");
+    }
+    return 0;
+}
