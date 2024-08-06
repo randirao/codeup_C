b0855aeda71338a9a286b395a6d0b15821a38db7
Han2 <randirao4245@gmail.com>
2024-08-06 23:42:43 +0900

    1365

--- 1365.c
+++ 1365.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 int main(){
     int n;
-    scanf("%d", &n);    //9
-    for(int i=0; i<n; i++){   //1 2 3 4 5 6 7 8 9
-        for(int j=0; j<n; j++){    //1 2 3 4 5 6 7 8 9
-            if( (i == 0) || (j == 0) || (i == n-1) || (j == n-1) || (i == j-1) || (i+j == n)){
-                //맨 위에 줄이거나 맨 앞이거나 맨 밑에 줄이거나 맨 뒤거나 열과 행이 같거나 
-                printf("*");
-            }
-            else printf(" ");   //아니면
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if( (i == 1) || (j == 1) || (i == n) || (j == n) || (i == j) || (i+j == n+1)) printf("*");
+            //맨 위에 줄이거나 맨 앞이거나 맨 밑에 줄이거나 맨 뒤거나 열과 행이 같거나 현재 열과 행이 n보다 1클 때
+            else printf(" ");
         }
         printf("\n");
         }
