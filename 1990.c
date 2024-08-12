19d4a3a9c65485078560c56946241e6351e98deb
Han2 <randirao4245@gmail.com>
2024-08-12 11:34:58 +0900

    1990

--- 1990.c
+++ 1990.c
@@ -3,11 +3,12 @@
 int main(){
     int sum = 0;
     char n[501];
-    scanf("%s", n);
-    for(int i=0; n[i]== '\n'; i++){
+    for(int i=0;; i++){
+        scanf("%c", &n[i]);
+        if(n[i]== '\n') break;
         sum += n[i] - '0';
     }
-    if (sum % 3 == 0) printf("%d", sum);
+    if (sum % 3 == 0) printf("1");
     else printf("0");
     return 0;
 }
