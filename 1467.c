19d19f3a8c9252acd4c9e16012100b25fd6443f0
Han2 <randirao4245@gmail.com>
2024-08-14 23:53:56 +0900

    1467

--- 1467.c
+++ 1467.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 int main(){
-    int n;
-    scanf("%d", &n);
-    
+    int n, m, a = 1, arr[101][101]={};
+    scanf("%d %d", &n, &m);
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            arr[i][j] = a;
+            a++;
+        }
+    }
+    for(int i=1; i<=n; i++){
+        for(int j=m; j>=1; j--){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
