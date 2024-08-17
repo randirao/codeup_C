747b994883abdcd36c0fa51b2eb9f93f270efa93
Han2 <randirao4245@gmail.com>
2024-08-18 00:42:03 +0900

    1474

--- 1474.c
+++ 1474.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 int main(){
-    int n, m, a=1, arr[101][101]={};
-    scanf("%d %d", &n, &m);
-    for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++) arr[i][j] = a++;
+    int n, m, a=1, sum=1, arr[101][101]={};
+    scanf("%d %d", &n, &m); 
+    for(int i=m; i>0; i--){    
+        if(a%2==0){
+            for(int j=1; j<=n; j++) arr[j][i] = sum++;
+        }
+        else{
+            for(int j=n; j>=1; j--) arr[j][i] = sum++;
+        }
+        a++;
     }
-    for(int i=n; i>=1; i--){
-        for(int j=m; j>=1; j--){
-            
+    for(int i=1; i<=n; i++){   
+        for(int j=1; j<=m; j++){   
+            printf("%d ", arr[i][j]);
         }
         printf("\n");
     }
