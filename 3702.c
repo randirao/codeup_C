d1389814ccde1657f97df7217c3107155a2b1bb2
Han2 <randirao4245@gmail.com>
2024-11-14 15:03:56 +0900

    3702

--- 3702.c
+++ 3702.c
@@ -4,7 +4,7 @@ int memo[51][51];
 
 int f(int r, int c){
     if(memo[r][c]) return memo[r][c];
-    if(r==1 || c==1) return memo[r][c] = 1;
+    if(r==1 || c==1) return memo[r][c] = memo[c][r] = 1;
     return memo[c][r] = memo[r][c] = (f(r-1, c) + f(r, c-1))%100000000;
 }
 int main(){
